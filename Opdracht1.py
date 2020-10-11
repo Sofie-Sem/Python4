2d97ebe2f0ecaca7e23ee17432cbf0488d01e921
Sofie Weetink <sofie.weetink@coderclass.nl>
2020-10-11 18:30:46 +0200

    Opdracht 1 klaar

---
+++ Opdracht1.py
@@ -0,0 +1,9 @@
+naam = input("Hoe heet je? ")
+print("-"*27)
+print(naam + ", wat leuk!")
+print("{:^27}".format(naam))
+print("Je naam is:{:>16}".format(naam))
+print("-"*27)
+                
+                
+                
---
+++ Opdracht2.py
@@ -0,0 +1,17 @@
+SCHERMBREEDTE = 32
+woord = "Hallo"
+
+print("+" + "-"*30 + "+")
+print("| Woord: {:" + str(SCHERMBREEDTE)+ "} |".format(woord))
+print("+" + "-"*30 + "+")
+
+#SCHERMBREEDTE = 60
+#MAX_WOORD_LENGTE = 14
+
+#woord = "1234567891012141619"
+
+#print(("| Woord: {:" + str(MAX_WOORD_LENGTE)+ "}").format(woord) + " "*(SCHERMBREEDTE - MAX_WOORD_LENGTE - 10) + "|")
+
+
+
+#print(("| Woord: {:" + str(MAX_WOORD_LENGTE)+ "}").format(woord) + " "*(SCHERMBREEDTE - MAX_WOORD_LENGTE - 10) + "|")
